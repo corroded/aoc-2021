68fb5e9fc588a9f9441eef5fec92618848a8ff87
Eumir Gaspar <eumirgaspar@gmail.com>
2021-12-01 23:10:19 +0800

    More formatting lol

--- day1/exercise1.ex
+++ day1/exercise1.ex
@@ -1,10 +1,13 @@
 {:ok, contents} = File.read("input1.txt")
 
-depths = contents |> String.split("\n", trim: true) |> Enum.map(fn x -> String.to_integer(x) end)
+depths = 
+  contents
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn x -> String.to_integer(x) end)
 
 depths
-|> Enum.with_index()
-|> Enum.count(fn {_, i} ->
-  Enum.at(depths, i) < Enum.at(depths, i + 1) && Enum.at(depths, i) != Enum.at(depths, -1)
+  |> Enum.with_index()
+  |> Enum.count(fn {_, i} ->
+    Enum.at(depths, i) < Enum.at(depths, i + 1) && Enum.at(depths, i) != Enum.at(depths, -1)
 end)
-|> IO.puts()
+  |> IO.puts()
