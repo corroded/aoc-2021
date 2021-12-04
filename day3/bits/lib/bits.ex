fc99d4921479a899cbb8d78c6f5a9338e1e31659
Eumir Gaspar <eumirgaspar@gmail.com>
2021-12-04 09:13:07 +0800

    More WIP

--- day3/bits/lib/bits.ex
+++ day3/bits/lib/bits.ex
@@ -3,18 +3,24 @@ defmodule Bits do
   Documentation for `Bits`.
   """
 
+  defstruct gamma_rate: 0, epsilon_rate: 0
+
   @bit_length = 12
 
-  def gamma_rate(bits, bit_length) do
+  def new(bits) do
+    %Bit{gamma_rate: 0, epsilon_rate: 0}
+  end
+
+  def gamma_rate_calc(bits, bit_length) do
     bits |> Enum.map(fn x -> String.at(x, 0) end) |> Enum.frequencies() |> Map.values()
   end
 
-  def gamma_rate(bits) do
+  def gamma_rate_calc(bits) do
     # gamma_rate(bits, @bit_length)
     [head | tail] = bits
   end
 
-  def epsilon_rate(bits) do
+  def epsilon_rate_calc(bits) do
     :world
   end
 
