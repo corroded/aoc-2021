fca4b2df8ac2da4b2e15dd359edbba8908c5ab34
Eumir Gaspar <eumirgaspar@gmail.com>
2021-12-12 05:10:35 +0800

    Working day6.1

--- submarine/lib/lanternfish.ex
+++ submarine/lib/lanternfish.ex
@@ -1,5 +1,29 @@
 defmodule Submarine.Lanternfish do
   def count(input, days) do
-    26
+    input
+    |> propagate(1, days)
+    |> Enum.count()
+  end
+
+  def propagate(input, day, days) when day < days do
+    input
+    |> Enum.flat_map(fn x -> breed(x) end)
+    |> propagate(day+1, days)
+  end
+
+  def propagate(input, day, days) when day == days do
+    input
+    |> Enum.flat_map(fn x -> breed(x) end)
+  end
+
+  defp breed(fish) do
+    cond do
+      fish >= 1 ->
+        [fish - 1]
+      fish == 0 ->
+        [6,8]
+      true ->
+        {:error, "Should not happen"}
+    end
   end
 end
--- submarine/test/lanterfish_test.exs
+++ submarine/test/lanterfish_test.exs
@@ -1,16 +1,35 @@
 defmodule SubmarineLanternfishTest do
   use ExUnit.Case
 
+  alias Submarine.Lanternfish, as: Lanternfish
+
   describe "#count" do
     setup [:with_example_txt]
 
     test "returns the count of lanterfish in x days", context do
-      assert Submarine.Lanternfish.count(context[:input], 18) == 26
-      assert Submarine.Lanternfish.count(context[:input], 80) == 5934
+      assert Lanternfish.count(context[:input], 18) == 26
+      assert Lanternfish.count(context[:input], 80) == 5934
+    end
+  end
+
+  describe "#propagate" do
+    test "it decreases internal timer of each fish" do
+      assert Lanternfish.propagate([3,4,3,1,2], 1, 1) == [2,3,2,0,1]
+    end
+
+    test "it increases fish for each 0 in it" do
+      assert (Lanternfish.propagate([2,3,2,0,1], 1, 1) -- [1,2,1,6,0,8]) == []
+      assert (Lanternfish.propagate([0,1,0,5,6,7,8], 1, 1) -- [6,0,6,4,5,6,7,8,8]) == []
+    end
+
+    test "it still works with multiple days" do
+      assert (Lanternfish.propagate([3,4,3,1,2], 1, 2) -- [1,2,1,6,0,8]) == []
+      assert (Lanternfish.propagate([3,4,3,1,2], 1, 7) -- [3,4,3,1,2,3,4,5,5,6]) == []
+      assert (Lanternfish.propagate([3,4,3,1,2], 1, 18) -- [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8]) == []
     end
   end
 
   defp with_example_txt(_) do
-    {:ok, [input: Submarine.read_input("test/fixtures/day6.txt")]}
+    {:ok, [input: Submarine.read_line("test/fixtures/day6.txt", true)]}
   end
 end
